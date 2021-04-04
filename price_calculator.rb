b82228c48f0eea78ad4f4977dbb166e455032f63
hpjaj <hmlevine@gmail.com>
2021-04-03 19:40:10 -0600

    Dedicated to run the program at the console level
    Provides full customization access of the store’s products, and the store’s sales.  Feel free to add/remove/update any items to customize the experience.
    
    If you do, be sure to update the “products” and “sales” arrays, if applicable.

---
+++ price_calculator.rb
@@ -0,0 +1,40 @@
+require_relative './models/product'
+require_relative './models/sale'
+require_relative './services/checkout_cart'
+
+
+###################################################################################
+##################################  Setup Store  ##################################
+###################################################################################
+
+milk   = Models::Product.new(name: 'Milk', price: 397)
+bread  = Models::Product.new(name: 'Bread', price: 217)
+banana = Models::Product.new(name: 'Banana', price: 99)
+apple  = Models::Product.new(name: 'Apple', price: 89)
+
+milk_sale  = Models::Sale.new(product_uid: milk.uid, quantity: 2, price: 500)
+bread_sale = Models::Sale.new(product_uid: bread.uid, quantity: 3, price: 600)
+
+products = [milk, bread, banana, apple]
+sales    = [milk_sale, bread_sale]
+
+
+###################################################################################
+##################################  Prompt User  ##################################
+###################################################################################
+
+puts 'Please enter all the items purchased separated by a comma'
+items = gets.chomp
+
+
+###################################################################################
+#############################  Checkout Their Buggy  ##############################
+###################################################################################
+
+cart = Services::CheckoutCart.new(products: products, sales: sales, items: items)
+
+if cart.process_items
+  cart.provide_receipt
+else
+  puts cart.error
+end
