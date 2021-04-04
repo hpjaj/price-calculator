4fe437705c984ae69541aae011f34f8d23cb67de
hpjaj <hmlevine@gmail.com>
2021-04-03 19:38:17 -0600

    Acts as the field general for the entire workflow
    Handles the orchestration of the process from initial input to providing a customer receipt.

---
+++ services/checkout_cart.rb
@@ -0,0 +1,58 @@
+require_relative './grocery_list'
+require_relative './compute'
+require_relative './receipt'
+require_relative '../helpers/products_helper'
+
+module Services
+  class CheckoutCart
+    include Helpers::ProductsHelper
+
+    attr_reader :products, :sales, :items, :error, :cart
+
+    # @param products [Array] A collection of the store's current Product instances
+    # @param sales [Array] A collection of the store's current Sale instances
+    # @param items [String] A comma separated string of grocery list items
+    #
+    def initialize(products:, sales: [], items:)
+      @products = products
+      @sales = sales
+      @items = items
+    end
+
+    def process_items
+      add_list_to_cart!
+      compute_totals
+      success?
+    rescue ArgumentError => e
+      set_error(e)
+    end
+
+    def provide_receipt
+      Services::Receipt.new(total: compute.total_price, savings: compute.total_savings, items: compute.items).generate
+    end
+
+  private
+
+    attr_reader :compute
+
+    def add_list_to_cart!
+      @cart = Services::GroceryList.new(items: items, products: products).add_to_cart!
+    end
+
+    def compute_totals
+      @compute = Services::Compute.new(products: products, sales: sales, cart: cart)
+
+      compute.totals
+    end
+
+    def success?
+      error.nil?
+    end
+
+    def set_error(e)
+      @error = e.message
+
+      success?
+    end
+  end
+end
